980e562ebfcb27a146758195617fe4c5cca0d2cd
dhruvin pandav <dhruvinpandav999@gmail.com>
2023-01-30 17:41:03 +0530

    DP | 30/1/23 6:00 PM

--- lib/global.dart
+++ lib/global.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-class Global
-{
-  static List Keybord= [
-    7,8,9,4,5,6,1,2,3,0
+
+class Global {
+  static String to = "IN";
+  static String from = "US";
+  static String toName = "INR";
+  static String fromName = "USD";
+
+  static TextEditingController Amount = TextEditingController();
+
+  static var Countrys = [
+    "INR",
+    "USD",
+    "EUR",
+    "AUD",
+    "PKR",
   ];
-
-  static String Currancy = "";
-
-  static String FirstCountry = "First Coutry";
-  static String SecondCountry = "Secound Coutry";
 }
